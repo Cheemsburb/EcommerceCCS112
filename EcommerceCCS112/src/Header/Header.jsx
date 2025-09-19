bbf5d346cbd812069858be87fccc51fa887cdef9
Gladwyn <sencilgladwyn@email.com>
2025-09-19 21:14:19 +0800

    feat: Completed Headers and Updated the assets

--- EcommerceCCS112/src/Header/Header.jsx
+++ EcommerceCCS112/src/Header/Header.jsx
@@ -1,22 +1,27 @@
 import style from "./Header.module.css";
 import logo from "../assets/Header/sun.png";
 import account from "../assets/Header/user.png";
-import search from "../assets/Header/Search.png";
+import search from "../assets/Header/search.png";
 import bag from "../assets/Header/bag.png";
 
 function Header() {
   return (
     <div className={style.body}>
-      <img src={logo} alt="Tribal Sun Logo" className={style.logo} />
-      <p className={style.storeName}> Perlas Silangan </p>
-      <p className={style.shortcuts}>HOME</p>
-      <p className={style.shortcuts}>BEST SELLERS</p>
-      <p className={style.shortcuts}>CATEGORIES</p>
-      <p className={style.shortcuts}>ABOUT US</p>
-      <div className={style.spacer}></div>
-      <img src={account} alt="User Account" className={style.icons} />
-      <img src={bag} alt="Cart" className={style.icons} />
-      <img src={search} alt="Search" className={style.icons} />
+      <div className={style.logoContainer}>
+        <img src={logo} alt="Tribal Sun Logo" className={style.logo} />
+        <p className={style.storeName}> Perlas Silangan </p>
+      </div>
+      <div className={style.shortcutContainer}>
+        <p className={style.shortcuts}>HOME</p>
+        <p className={style.shortcuts}>BEST SELLERS</p>
+        <p className={style.shortcuts}>CATEGORIES</p>
+        <p className={style.shortcuts}>ABOUT US</p>
+      </div>
+      <div className={style.iconsContainer}>
+        <img src={account} alt="User Account" className={style.icons} />
+        <img src={bag} alt="Cart" className={style.icons} />
+        <img src={search} alt="Search" className={style.icons} />
+      </div>
     </div>
   );
 }
