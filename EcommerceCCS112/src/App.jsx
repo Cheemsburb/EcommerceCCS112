d1a309c5e44f3d32566cdbb26ca9249ad6900797
Gladwyn <sencilgladwyn@email.com>
2025-09-20 21:14:47 +0800

    feat: User login, register and profile set up, along with some routing

--- EcommerceCCS112/src/App.jsx
+++ EcommerceCCS112/src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 //Components
 import LandingPage from "./LandingPage/Main/LandingPage.jsx";
+
+//Routes
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
       <div className="body">
         <LandingPage />
       </div>
-    </>
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
